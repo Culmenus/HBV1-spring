1f2ea61ca623d5de16a9370484913bf827d05b36
Daníel Helgi Ágústsson <danielhelgi1@gmail.com>
2021-10-31 22:56:19 +0000

    import

--- src/main/java/bakendi/restful/controller/HomeController.java
+++ src/main/java/bakendi/restful/controller/HomeController.java
@@ -1,5 +1,6 @@
 package bakendi.restful.controller;
 
+import bakendi.restful.persistence.entities.User;
 import bakendi.restful.service.ForumService;
 import bakendi.restful.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 
@@ -22,7 +24,7 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public void HomeController(HttpServletResponse response,HttpSession session) throws IOException {
+    public void HomeController(HttpServletResponse response, HttpSession session) throws IOException {
         User user = (User) session.getAttribute("loggedInUser");
         if(user == null)
             response.sendRedirect("/login");
