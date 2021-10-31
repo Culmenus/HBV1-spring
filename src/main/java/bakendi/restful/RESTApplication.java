acb36d735bef134f493fd70b7e6f66acf03db4cc
jongvnnar <jgh060@gmail.com>
2021-10-31 12:28:27 +0000

    refactor restful

--- src/main/java/bakendi/restful/RESTApplication.java
+++ src/main/java/bakendi/restful/RESTApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ResTfulApplication {
+public class RESTApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ResTfulApplication.class, args);
+        SpringApplication.run(RESTApplication.class, args);
     }
 }
